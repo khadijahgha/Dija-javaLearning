f16f4350deecafb729adcdfe72919d77ab424e03
khadijahgha <khadijahgha@gmail.com>
2026-08-04 15:34:13 +0000

    init commit

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -26,8 +26,34 @@ public class Main {
                 "Canada"
         );
 
-         student1.displayStudent();
+         /*student1.displayStudent();
          student2.displayStudent();
-         student3.displayStudent();
+         student3.displayStudent();*/
+
+        if(student2.isExcellentStudent()){
+            System.out.println(student2.getName() + " is an excellent student.");
+        }else{
+            System.out.println(student2.getName() + " is not excellent student.");
+        }
+
+        System.out.println("Student number 1 details");
+        System.out.println("-------------------------");
+        System.out.println(student1.getName());
+        student1.setGpa(3.5);
+        System.out.println(student1.getGpa());
+        System.out.println("**************************");
+
+        System.out.println("Student number 2 details");
+        System.out.println("-------------------------");
+        System.out.println(student2.getName());
+        student2.setGpa(4.5);
+        System.out.println(student2.getGpa());
+        System.out.println("**************************");
+
+        System.out.println("Student number 3 details");
+        System.out.println("-------------------------");
+        System.out.println(student3.getName());
+        student3.setGpa(2.0);
+        System.out.println(student3.getGpa());
     }
 }
--- src/oop/Student.java
+++ src/oop/Student.java
@@ -1,28 +1,52 @@
 package oop;
 
 public class Student {
-    String name;
-    int age;
-    String course;
-    double gpa;
-    String country;
+    private String name;
+    private int age;
+    private String course;
+    private double gpa;
+    private String country;
 
     //constructors
-
     Student(String name, int age, String course,double gpa, String country){
         this.name = name;
-        this.age =age;
+        this.age = age;
         this.course = course;
         this.gpa = gpa;
         this.country = country;
     }
 
     //method
-    public void displayStudent(){
+        public void displayStudent(){
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Course: " + course);
         System.out.println("GPA: " + gpa);
         System.out.println("Country: " + country);
     }
+
+    //method
+    public boolean isExcellentStudent(){
+     return gpa >= 3.5;
+    }
+
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+
+    //Setters
+    public void setGpa(double gpa){
+        if(gpa >= 2.5 && gpa <= 4.5){
+            this.gpa = gpa;
+        }else{
+            System.out.println("Invalid gpa! gpa must be between 2.5 and 4.5");
+        }
+    }
 }
+
+
+
